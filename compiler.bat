7d84781a91e9025cbd999c207ef84f8b2db00f1f
MrBooks36 <mrbooks36@outlook.com>
2025-10-16 18:58:52 +1000

    Add -w flag to PyCompyle commands in compiler.bat
    Updated compiler.bat to include the -w flag when compiling packupdater.py and updaterinstaller.py to enable windowed mode for the generated executables. Also updated the corresponding .exe files.

--- compiler.bat
+++ compiler.bat
@@ -1,5 +1,5 @@
 @echo off
 
-python -m PyCompyle "C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\packupdater.py" -nc -i="C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\pack.ico"
+python -m PyCompyle "C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\packupdater.py" -nc -i="C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\pack.ico" -w
 
-python -m PyCompyle "C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\updaterinstaller.py" -nc -i="C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\pack.ico"
+python -m PyCompyle "C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\updaterinstaller.py" -nc -i="C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\pack.ico" -w
