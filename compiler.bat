8b0ec3eac6181dd5b0ec39fda2161dfd3271a818
MrBooks <mrbooks36@outlook.com>
2026-03-06 21:15:44 +1000

    Update compiler.bat

--- compiler.bat
+++ compiler.bat
@@ -1,3 +1,3 @@
 @echo off
 
-python -m PyCompyle "C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\packupdater.py" -nc -i="C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\pack.ico" -p="encodings.idna"
+python -m PyCompyle "C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\packupdater.py" -nc -i="C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\pack.ico" -p="encodings.idna" -p=gzip
